1952b1ad916bdaafaae4abf0e295c724a567a234
Toby Twigger <tobytwigger1@gmail.com>
2020-06-08 18:03:47 +0100

    UPdate migration name

--- database/migrations/2020_16_08_175446_set_cell_value_column_to_nullable.php
+++ database/migrations/2020_16_08_175446_set_cell_value_column_to_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDataEntryCellTable extends Migration
+class SetCellValueColumnToNullable extends Migration
 {
     /**
      * Run the migrations.
